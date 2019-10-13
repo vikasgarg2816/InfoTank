ce042a8879a8658952e3886d95cccc17d85314c0
Amar Jain <amarjain9205251694@gmail.com>
2019-10-13 23:37:02 +0530

    icon added

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -19,7 +19,7 @@ module.exports.home = async function(req, res){
     
         let users = await User.find({});
         return res.render('home', {
-            title: "Codeial | Home",
+            title: "InfoTank | Home",
             posts:  posts,
             all_users: users
         });
